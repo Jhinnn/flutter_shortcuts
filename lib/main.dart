540137354550614d9b4daec25ccb6a19ee352e9d
Jhinnn <git config --global user.email you@example.com>
2023-03-16 13:53:04 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  FocusNode focusNode = FocusNode();
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    focusNode.requestFocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Shortcuts(
+        shortcuts: <LogicalKeySet, Intent>{
+          LogicalKeySet(LogicalKeyboardKey.control, LogicalKeyboardKey.keyO):
+              const NumberIncIntent(),
+          LogicalKeySet(LogicalKeyboardKey.control, LogicalKeyboardKey.keyP):
+              const NumberDesIntent(),
+        },
+        child: Actions(
+          //Actions组件将意图映射到操作
+          actions: <Type, Action<Intent>>{
+            NumberIncIntent: IncNumberAction(incFunc: () {
+              setState(() {
+                _counter++;
+              });
+            }),
+            NumberDesIntent: DesNumberAction(desFunc: () {
+              setState(() {
+                _counter--;
+              });
+            })
+          },
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Focus(
+                  focusNode: focusNode,
+                  child: Text(
+                    '$_counter',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class NumberIncIntent extends Intent {
+  const NumberIncIntent();
+}
+
+class NumberDesIntent extends Intent {
+  const NumberDesIntent();
+}
+
+class IncNumberAction extends Action<NumberIncIntent> {
+  IncNumberAction({required this.incFunc});
+  final Function incFunc;
+
+  @override
+  Object? invoke(NumberIncIntent intent) {
+    print('Increase.......');
+
+    incFunc();
+
+    return null;
+  }
+}
+
+class DesNumberAction extends Action<NumberDesIntent> {
+  DesNumberAction({required this.desFunc});
+  final Function desFunc;
+
+  @override
+  Object? invoke(NumberDesIntent intent) {
+    print('Descrease.......');
+
+    desFunc();
+    return null;
+  }
+}
+
